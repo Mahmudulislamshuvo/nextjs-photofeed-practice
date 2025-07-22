938cd498f57c516afecee4bb6c14b25193af9e77
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-07-23 03:51:43 +0600

    8.12 with middleware internationalization

---
+++ middleware.js
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+
+const { match } = require("@formatjs/intl-localematcher");
+const Negotiator = require("negotiator");
+
+let locales = ["en", "bn"];
+let defultLocale = "en";
+
+function getLocale(request) {
+  const acceptedLanguage = request.headers.get("accept-language") ?? undefined;
+  let headers = { "accept-language": acceptedLanguage };
+  let languages = new Negotiator({ headers }).languages();
+
+  console.log(languages);
+
+  return match(languages, locales, defultLocale);
+}
+
+export function middleware(request) {
+  const pathname = request.nextUrl.pathname;
+
+  const pathnameIsMissingLocale = locales.every(
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  );
+
+  if (pathnameIsMissingLocale) {
+    const locale = getLocale(request);
+
+    // e.g. incoming request is /products
+    // The new URL is now /en-US/products
+    return NextResponse.redirect(
+      new URL(`/${locale}/${pathname}`, request.url)
+    );
+  }
+}
+
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next, assets, api)
+    "/((?!api|assets|.*\\..*|_next).*)",
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
+};
