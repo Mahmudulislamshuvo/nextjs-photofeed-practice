5265af36bbd046007c6595c933c96bc3b03a92ca
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-07-23 11:36:39 +0600

    with problem

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
         <Navbar />
         <div className="container my-4 lg:my-8"> {children}</div>
