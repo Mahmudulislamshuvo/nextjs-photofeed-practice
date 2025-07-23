2e5c384b234a67ffad64adf057a8f565fd9dda6a
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-07-23 06:56:33 +0600

    Pic details 8.13

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,6 +1,15 @@
+import PhotoList from "@/components/PhotoList";
 import { getDictionary } from "./dictionaries";
 
 export default async function Home({ params: { lang } }) {
   const dist = await getDictionary(lang);
-  return <div>{dist.save}</div>;
+  const response = await fetch(`${process.env.BASE_URL}/photos`);
+
+  const photos = await response.json();
+
+  return (
+    <div>
+      <PhotoList photos={photos} />
+    </div>
+  );
 }
